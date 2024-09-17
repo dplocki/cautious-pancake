42719924d8d1c3b995e108f0d1f89d99d62acbee
dplocki <dawid.plocki@gmail.com>
2024-09-17 22:23:38 +0200

    style: remove style sheet files

--- src/App.jsx
+++ src/App.jsx
@@ -88,7 +88,7 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-  mainView: { flex: 1, padding: 20 },
+  mainView: { flex: 1, padding: 20, innerWidth: '100%' },
   maxQouteView: { flexDirection: 'row', marginBottom: 20 },
   edit: { flex: 1, borderWidth: 1, padding: 10, marginRight: 10 },
   itemList: { flex: 1 },
