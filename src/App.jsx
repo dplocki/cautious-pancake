c2ab4e8cc17bbf983915770e7596e126d083c501
dplocki <dawid.plocki@gmail.com>
2024-09-18 19:40:47 +0200

    style(app): remove margin from max qoute settings

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,7 @@ const App = () => {
 const styles = StyleSheet.create({
   mainView: { flex: 1, padding: 20, innerWidth: '100%' },
   maxQouteView: { flexDirection: 'row', marginBottom: 20 },
-  edit: { flex: 1, borderWidth: 1, padding: 10, marginRight: 10 },
+  edit: { flex: 1, borderWidth: 1, padding: 10 },
   itemList: { flex: 1 },
   item: { padding: 20, backgroundColor: '#f0f0f0', marginVertical: 5 },
   addItemView: { flexDirection: 'row', marginTop: 20 },
