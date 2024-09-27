292bb6b044ed9145ca67e66f49282ab09c237de8
dplocki <dawid.plocki@gmail.com>
2024-09-27 16:09:49 +0200

    test(knapsack.problem): add unit test for all elements match

---
+++ __tests__/knapsack.problem.test.js
@@ -0,0 +1,18 @@
+import knapsackProblem from "../src/knapsack.problem";
+
+function transformArrayIntoItemArray(values) {
+  return values.map((value, index) => ({
+    value,
+    id: index.toString(),
+    isSelected: false,
+  }));
+}
+
+test('all match', () => {
+  const qoutes = [3, 5, 2];
+  const inputItems = transformArrayIntoItemArray(qoutes);
+  const [resultSum, resultItems] = knapsackProblem(11, inputItems);
+
+  expect(resultSum).toEqual(10);
+  expect(qoutes.sort()).toEqual(resultItems.map(i => i.value).sort());
+});
