a93a2d184dbc4f44d813ce16f6f4cbb91d7f3027
dplocki <dawid.plocki@gmail.com>
2024-09-28 22:45:42 +0200

    refactor(quoteslist): remove unused code

--- src/QuotesList.jsx
+++ src/QuotesList.jsx
@@ -1,12 +1,6 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-import {
-  View,
-  TextInput,
-  Button,
-  FlatList,
-  StyleSheet
-} from 'react-native';
+import { FlatList } from 'react-native';
 import ListItem from './ListItem';
 import knapsackProblem from './knapsack.problem';
 import NumberInput from './NumberInput';
@@ -37,7 +31,7 @@ const QuoteList = ({ maxQuote }) => {
       ...list,
       {
         id: Date.now().toString(),
-        value: list.find(i => i.id === id).value,
+        value: list.find(item => item.id === id).value,
         isSelected: false,
       }
     ]);
@@ -51,19 +45,11 @@ const QuoteList = ({ maxQuote }) => {
         data={list}
         renderItem={({ item }) => <ListItem item={item} onRemove={handleRemoveItem} onCopy={handleCopyItem} />}
         keyExtractor={item => item.id}
-        style={styles.itemList}
       />
       <NumberInput onConfirm={handleAddItem} placeholder={"Add to list"} />
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  edit: { flex: 1, borderWidth: 1, padding: 10 },
-  addItemView: { flexDirection: 'row', marginTop: 20 },
-  itemList: { flex: 1 },
-  edit: { flex: 1, borderWidth: 1, padding: 10 },
-});
 
 QuoteList.propTypes = {
   maxQuote: PropTypes.number.isRequired,
