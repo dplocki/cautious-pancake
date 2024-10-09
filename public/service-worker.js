ffee0cadbd9981e0cda4838e76e4c2aa29eb8707
dplocki <dawid.plocki@gmail.com>
2024-10-09 18:36:16 +0200

    feat(service-worker): simplified the code for test

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,7 @@
 const CACHE_NAME = 'cautious-pancake-v1.0.0';
-const urlsToCache = [
+const URLS_TO_CACHE = [
   '/',
   '/index.html',
-  '/styles.css',
-  '/main.js',
   '/android-chrome-192x192.png',
   '/android-chrome-512x512.png',
   '/apple-touch-icon.png',
@@ -12,28 +10,12 @@ const urlsToCache = [
   '/favicon.ico',
 ];
 
-self.addEventListener('install', event => {
-  caches.open(CACHE_NAME).then(cache => {
-    console.log('Opened cache');
-    return cache.addAll(urlsToCache);
-  });
-});
-
-self.addEventListener('fetch', event => {
-  caches.match(event.request).then(response => {
-    return response || fetch(event.request);
-  })
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  caches.keys().then(cacheNames => {
-    return Promise.all(
-      cacheNames.map(cacheName => {
-        if (cacheWhitelist.indexOf(cacheName) === -1) {
-          return caches.delete(cacheName);
-        }
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(URLS_TO_CACHE);
       })
-    );
-  })
+  );
 });
