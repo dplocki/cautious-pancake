3424c45f699c1161d453d5ba20556d23a77103c6
dplocki <dawid.plocki@gmail.com>
2024-10-13 09:01:08 +0200

    feat(webmanifest): move the manifest file into the vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'android-chrome-192x192.png',
+        'android-chrome-512x512.png',
+        'apple-touch-icon.png',
+        'favicon.ico',
+        'favicon-16x16.png',
+        'favicon-32x32.png',
+      ],
+      manifest: {
+        "background_color": "#FFFFFF",
+        "description": "An pancake and the cautious one",
+        "display": "standalone",
+        "icons": [
+          {
+            "src": "/android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/android-chrome-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ],
+        "name": "Cautious Pancake",
+        "scope": ".",
+        "short_name": "Cautious Pancake",
+        "start_url": "./",
+        "theme_color": "#FFFFFF"
+      }
+    }),
+  ],
   resolve: {
     alias: {
       'react-native': 'react-native-web'
