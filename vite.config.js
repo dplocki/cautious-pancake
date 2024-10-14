862926b5791e8241470c56cd570e9cc85e51922f
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2024-10-14 09:58:08 +0200

    feat(vite-config): update the manifest (all icons)

--- vite.config.js
+++ vite.config.js
@@ -16,26 +16,46 @@ export default defineConfig({
         'favicon-32x32.png',
       ],
       manifest: {
-        "background_color": "#FFFFFF",
-        "description": "An pancake and the cautious one",
-        "display": "standalone",
-        "icons": [
+        background_color: '#FFFFFF',
+        description: 'An pancake and the cautious one',
+        display: 'standalone',
+        icons: [
           {
-            "src": "/android-chrome-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            "src": "/android-chrome-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png"
-          }
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon.ico',
+            sizes: '48x48',
+            type: 'image/x-icon',
+          },
         ],
-        "name": "Cautious Pancake",
-        "scope": ".",
-        "short_name": "Cautious Pancake",
-        "start_url": "./",
-        "theme_color": "#FFFFFF"
+        name: 'Cautious Pancake',
+        scope: '.',
+        short_name: 'Cautious Pancake',
+        start_url: './',
+        theme_color: '#FFFFFF',
       },
       filename: 'service-worker.js',
       injectRegister: false,
@@ -43,10 +63,12 @@ export default defineConfig({
         runtimeCaching: [
           {
             urlPattern: ({ request }) => {
-              return request.destination === 'document'
-                || request.destination === 'image'
-                || request.destination === 'script'
-                || request.destination === 'style';
+              return (
+                request.destination === 'document' ||
+                request.destination === 'image' ||
+                request.destination === 'script' ||
+                request.destination === 'style'
+              );
             },
             handler: 'CacheOnly',
             options: {
@@ -61,7 +83,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      'react-native': 'react-native-web'
-    }
-  }
+      'react-native': 'react-native-web',
+    },
+  },
 });
