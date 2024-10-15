778c6eb92eb0cc89cb13900a1baba50a592b2917
dplocki <dawid.plocki@gmail.com>
2024-10-15 18:31:52 +0200

    feat(app): add styles for safe-area-view container

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { SafeAreaView } from 'react-native-web';
+import { SafeAreaView, StyleSheet } from 'react-native-web';
 import App from './App.jsx'
 import './index.css'
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register("/service-worker.js", { scope: "/" })
-    .then(registration => {
-      if (registration.installing) {
-        console.log("Service worker installing");
-      } else if (registration.waiting) {
-        console.log("Service worker installed");
-      } else if (registration.active) {
-        console.log("Service worker active");
-      }
-    })
     .catch(error => {
       console.error(`Registration failed with ${error}`);
     });
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <App />
     </SafeAreaView>
   </StrictMode>,
